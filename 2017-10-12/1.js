98ac86e9deaa26542b006f844a00e133475a1d6a
nizp <nzp890524@163.com>
2017-10-12 16:00:40 +0800

    down

--- 2017-10-12/1.js
+++ 2017-10-12/1.js
@@ -1,1 +1,8 @@
 /* add by chen-s-y */
+var box = document.getElementById('box');
+box.addEventListener('mousedown',down);
+function down(ev){
+	var disX = ev.clientX - this.offsetLeft;
+	var disY = ev.clientY - this.offsetTop;
+	
+}
