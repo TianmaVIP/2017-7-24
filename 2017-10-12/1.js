ff38b9d941dc14c825711ffc31e6eb164e38c7aa
nizp <nzp890524@163.com>
2017-10-12 16:05:28 +0800

    move

--- 2017-10-12/1.js
+++ 2017-10-12/1.js
@@ -1,1 +1,7 @@
 /* add by chen-s-y */
+
+document.addEventListener('mousemove',move);
+function move(ev){
+	box.style.left = ev.clientX - disX + 'px'; 
+	box.style.top = ev.clientY - disY + 'px';
+}
